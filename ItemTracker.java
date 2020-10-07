702bdf429419098e43c75a70d03314830da89e12
martintomazic <martin.tomazic97@gmail.com>
2020-10-07 21:36:17 +0200

    adding gradle to .gitinit

--- ItemTracker.java
+++ ItemTracker.java
@@ -7,12 +7,12 @@ public class ItemTracker {
         List<String> items = new ArrayList<>();
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()){
-            args = sc.nextLine().split(" ");
-            if (args[0].equals("add")){
-                items.add(args[1]);
-            } else if (args[0].equals("remove")) {
-                items.remove(args[1]);
-            } else if (args[0].equals("display")){
+            String[] nextLine = sc.nextLine().split(" ");
+            if (nextLine[0].equals("add")){
+                items.add(nextLine[1]);
+            } else if (nextLine[0].equals("remove")) {
+                items.remove(nextLine[1]);
+            } else if (nextLine[0].equals("display")){
                 System.out.println(items.toString());
             } else {
                 System.out.println("wrong command");
